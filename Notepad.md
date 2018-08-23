9fead6a4ae391817dbf9417d630a6b5a450cfe09
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-08-23 16:46:30 +0200

    Add generic ingredients

---
+++ Notepad.md
@@ -0,0 +1,11 @@
+#Notepad
+
+## Transforming Object into Array 
+const object1 = {
+  a: 'somestring',
+  b: 42,
+  c: false
+};
+
+console.log(Object.keys(object1));
+// expected output: Array ["a", "b", "c"]
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,11 +5,19 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      becon: 1,
+      cheese: 2,
+      meat: 2,
+    }
+  }
   render() {
     return (
       <div>
         <Aux>
-          <Burger />
+          <Burger ingredients={this.state.ingredients} />
           <div>Build controll</div>
         </Aux>
       </div>
