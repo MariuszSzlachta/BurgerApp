35b2f1e5131a092197b24b0926371ce9d2905885
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-11-28 20:17:53 +0100

    Create DrawerToogle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -14,10 +14,16 @@ class Layout extends Component {
     this.setState({ showSideDrawer: false })
   }
 
+  sideDrawerToogleHandler = () => {
+    this.setState(prevState => ({
+      showSideDrawer: !prevState.showSideDrawer
+    }))
+  }
+
   render() {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToogleHandler} />
         <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
         <main className={classes.Content}>
           { this.props.children }
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import NavigationItems from '../NavigationItems/NavigationIntems';
 import Logo from '../../Logo/Logo';
+import DrawerToogle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
 import classes from './Toolbar.css';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToogle drawerToggleClicked={props.drawerToggleClicked} />
     <div className={classes.Logo}>
       <Logo />
     </div>
