c606adb345fcc54f263ba10ffdecbe1aea37f0af
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-12-11 15:41:48 +0100

    Add CheckoutSummary component and Checkout container

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+class Checkout extends Component {
+  state = {
+    ingrediets: {
+      salad: 1,
+      meet: 1,
+      bacon: 1,
+      cheese: 1
+    }
+  }
+  render() {
+    return (
+      <div>
+        <CheckoutSummary ingredients={this.state.ingrediets} />
+      </div>
+    );
+  }
+}
+
+export default Checkout;
