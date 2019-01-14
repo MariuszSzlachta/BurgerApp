5a17577a60a3c3358c0246c0dc1def0a7c19d3ac
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-01-14 13:34:56 +0100

    Create basic version of reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,17 +1,34 @@
 import { ADD_INGREDIENT, REMOVE_INGREDIENT } from './actions';
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
 
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    chease: SSL_OP_SSLEAY_080_CLIENT_DH_BUG,
+    meat: 0
+  },
   totalPrice: 4
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_INGREDIENT:
-      const ingredients = [...state.ingredients];
-      
       return {
         ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        }
+      }
+    
+    case REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }
       }
 
     default:
