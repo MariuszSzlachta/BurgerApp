de36fc30ca02b44f5c76be21dc7cb1d0877d4db7
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-01-24 21:14:28 +0100

    Create actions for fetching orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -4,6 +4,15 @@ export const PURCHASE_BURGER_SUCCESS = 'PURCHASE_BURGER_SUCCESS';
 export const PURCHASE_BURGER_FAIL = 'PURCHASE_BURGER_FAIL';
 export const PURCHASE_BURGER_START = 'PURCHASE_BURGER_START';
 export const PURCHASE_INIT = 'PURCHASE_INIT';
+export const FETCH_ORDERS_START = 'FETCH_ORDERS_START';
+export const FETCH_ORDERS_SUCCESS = 'FETCH_ORDERS_SUCCESS';
+export const FETCH_ORDERS_FAILED = 'FETCH_ORDERS_FAILED';
+
+export const purchaseBurgerStart = () => {
+  return {
+    type: PURCHASE_BURGER_START
+  }
+}
 
 export const purchaseBurgerSuccess = (orderID, orderData) => {
   return {
@@ -20,11 +29,6 @@ export const purchaseBurgerFail = error => {
   }
 }
 
-export const purchaseBurgerStart = () => {
-  return {
-    type: PURCHASE_BURGER_START
-  }
-}
 
 export const purchaseBurgerRequest = (orderData) => {
   return dispatch => {
@@ -39,8 +43,48 @@ export const purchaseBurgerRequest = (orderData) => {
   }
 }
 
+
 export const purchaseInit = () => {
   return {
     type: PURCHASE_INIT
+  }
+}
+
+export const fetchOrderStart = () => {
+  return {
+    type: FETCH_ORDERS_START
+  }
+}
+
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: FETCH_ORDERS_SUCCESS,
+    orders
+  }
+}
+
+export const fetchOrdersFailed = (error) => {
+  return {
+    type: FETCH_ORDERS_FAILED,
+    error
+  }
+}
+
+export const fetchOrdersRequest = () => {
+  return dispatch => {
+    axios.get('/orders.json')
+      .then(res => {
+        const fetchedOrders = [];
+        for ( let key in res.data){
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          })
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch(err => {
+        dispatch(fetchOrdersFailed(err))
+      })
   }
 }
